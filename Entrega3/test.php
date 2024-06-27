326ba243ff6374efa16bf077854e7a62c16303bb
DeveLoopzz <develoopzz00@gmail.com>
2024-06-27 21:38:30 +0200

    Exercise 1 variable and constant

--- Entrega3/test.php
+++ Entrega3/test.php
@@ -1,3 +1,12 @@
 <?php 
 
-echo "hello World";
+    $int = 1;
+    $decimal = 1.2;
+    $text = "text";
+    $boleano = true;
+
+    echo $int, $decimal, $text, $boleano;
+
+    define("miNombre", "Sergio");
+
+    echo miNombre;
