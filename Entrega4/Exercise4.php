508b0225815cb47a6415d63b8a0950f7a3f20400
DeveLoopzz <develoopzz00@gmail.com>
2024-06-28 21:40:47 +0200

    Adding comments

--- Entrega4/Exercise4.php
+++ Entrega4/Exercise4.php
@@ -1,5 +1,10 @@
 <?php 
 
+//CREATION OF A ASSOCIATIVE ARRAY
 $human = array("nombre" => "Sergio", "edad" => 24, "email" => "develoopzz00@gmail.com", "comidaFav" => "Pasta");
 
-var_dump($human);
+var_dump($human) ;
+
+
+//SIMPLE DEMOSTRATION HOW TO ACCES TO A SINGLE VALUE OF THE ASSOCIATIVE ARRAY
+echo "<br>" .$human ["nombre"];
