a6313012dd233a243b484265662be50459f58373
DeveLoopzz <develoopzz00@gmail.com>
2024-07-02 10:41:17 +0200

    Exercise1 full version

---
+++ Entrega7/Exercise1.php
@@ -0,0 +1,6 @@
+<?php
+
+$name = $_REQUEST["name"];
+$username = $_REQUEST["username"];
+
+echo $name . "<br>" . $username;
---
+++ Entrega7/index.php
@@ -0,0 +1,16 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Form</title>
+        <meta charset ="utf8">
+    </head>
+    <body>
+        <form action="Exercise1.php" method="post">
+            <p>Nombre:<input type="text" name="name"></p>
+            <p>Username:<input type="text" name="username"></p>
+            <p><input type="submit" value ="enviar"></p>
+        </form>
+    </body> 
+</html>
