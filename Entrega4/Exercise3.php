72e350af869227f24314ad39c7a664a02724eea3
DeveLoopzz <develoopzz00@gmail.com>
2024-07-03 11:20:06 +0200

    Function optimitzacion

--- Entrega4/Exercise3.php
+++ Entrega4/Exercise3.php
@@ -5,12 +5,11 @@ $palabras = ["abcd","abcd","abcd"];
 echo isFoundCharacter($palabras, "a" );
 
 function isFoundCharacter($array, $char) : bool{
-    $response = true;
         foreach($array as $word){
             if(stripos($word, $char) === false){
-                $response = false;
+                return false;
             }
         }
-    return $response;
+    return true;
 }
 //STRIPOS INSTEAD OF STRPOS FOR IGNORE UPPER OR LOWER CASE CHARACTER USEFULL!!!!!!!!
