b92dcf702a971f2c4fb6c54d6c3187e0f4089813
DeveLoopzz <develoopzz00@gmail.com>
2024-07-03 21:53:37 +0200

    LVL3 of Entrega3 full version

--- Entrega3/LVL3/Exercise1.php
+++ Entrega3/LVL3/Exercise1.php
@@ -1,18 +1,34 @@
 <?php
 
-$num =23;
+$arrayNums = [];
 
-$numsPrimers = [];
+//GENERO NUMEROS DEL 1 AL 100 EN EL ARRAY CREADO ARRIBA
+for($i = 1; $i <= 1000; $i++){
+    $arrayNums[] = $i;
+}
 
-erastotenes($num);
+//PASO LA FUNCION A UN ARRAY NUEVO QUE POBLARE CON LOS NUMEROS PRIMEROS
+$numsPrimers = erastotenes100($arrayNums);
 
+//IMPRIMO EL ARRAY COMPLETO PARA VER LOS RESULTADOS, FUTURAMENTE CAMBIAR POR UN FOR EACH
 print_r($numsPrimers);
 
-
-function erastotenes($num){
-    for($i = 2; $i < $num; $i++){
-            if($num % $i != 0){
-                $numsPrimers[] = $num;
+//DADO UN LIMITE DE 100 CARACTERES
+function erastotenes100(array $arrayNums) : array{
+    $arrayDelete= [];
+    $numMax = max($arrayNums);
+    for($i = 2; ($i**2) < $numMax; $i++) {
+        if(!in_array($i, $arrayDelete)){
+            for($j = 2; ($i * $j) <= $numMax; $j++){
+                $numDelete = ($i * $j);
+                $arrayDelete[] = $numDelete;
             }
+            }
+        }
+        foreach($arrayDelete as $num) {
+            $index = array_search($num, $arrayNums);
+            unset($arrayNums[$index]);
+        }
+        
+        return $arrayNums = array_values($arrayNums);
     }
-}
